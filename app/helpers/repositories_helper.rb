1db746623580e30a91a0d64d7723322fb7f305e4
Lihui Qin <QLH@Lihuis-MacBook-Air.local>
2016-05-21 09:07:24 -0400

    Merge with filter

--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,2 +1,14 @@
 module RepositoriesHelper
+	def class_for_severity(severity)
+		color_map = {'Low' => 'info', 'Medium' => 'warning', 'High' => 'danger'}
+		color_map[severity]
+	end
+
+	def selected_severity_level
+		if params[:severity].present? and ["High","Medium","Low"].include?(params[:severity])
+			return params[:severity]
+		else
+			return "Severity Level"
+		end
+	end
 end
