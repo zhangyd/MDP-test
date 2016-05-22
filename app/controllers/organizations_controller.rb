a40c8b8d86b88499eb09d494c5982cc3363eaa7a
zhangyd <zhangyd@umich.edu>
2016-05-22 12:54:26 -0400

    delete cascade organization->repository->reports

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -34,7 +34,24 @@ class OrganizationsController < ApplicationController
   end
 
   def destroy
+
+    repos = Repository.where(:organization_id => @organization.id)
+
+    for i in 0..(repos.count - 1)
+      repopath = repos[i].repopath
+      cmd = "rm -r #{repopath}"
+      system cmd
+
+      reports = Report.where(:repository_id => repos[i].id)
+      for i in 0..(reports.count - 1)
+        report = reports[i].filename
+        cmd = "rm -r #{report}"
+        system cmd
+      end
+    end
+
     @organization.destroy
+
     respond_with(@organization)
   end
 
